12924e1edd472b2b4d1c47e93d2e2c27fbbd924c
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-07-31 21:18:18 -0600

    Revert "Regex"

--- SwiftExpression/SwiftExpression/SwiftExpression.h
+++ SwiftExpression/SwiftExpression/SwiftExpression.h
@@ -15,3 +15,5 @@ FOUNDATION_EXPORT double SwiftExpressionVersionNumber;
 FOUNDATION_EXPORT const unsigned char SwiftExpressionVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SwiftExpression/PublicHeader.h>
+
+
