fdbbf0f4a59c52d4f843434eb85ddede3f32dd42
Joshua Alvarado <joshua@mondorobot.com>
2016-08-10 15:31:01 -0600

    Revert "Revert "Regex""
    This reverts commit 51ba29f9a81c2817778d743f73b0604716e21c19.

--- SwiftExpression/SwiftExpression/SwiftExpression.h
+++ SwiftExpression/SwiftExpression/SwiftExpression.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double SwiftExpressionVersionNumber;
 FOUNDATION_EXPORT const unsigned char SwiftExpressionVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SwiftExpression/PublicHeader.h>
-
-
