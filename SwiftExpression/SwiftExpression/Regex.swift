43e5c3dce22000ca03fdb569fee618224f827740
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-08-12 01:19:46 -0600

    Added documentation

--- SwiftExpression/SwiftExpression/Regex.swift
+++ SwiftExpression/SwiftExpression/Regex.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
+/**
+ Regex is a struct that is built off of `NSRegularExpression` to pattern match with. It is to be used to work with Regular Expression with `Strings`. Please refer to `NSRegularExpression` for the regular expression characters and [http://userguide.icu-project.org/strings/regexp](http://userguide.icu-project.org/strings/regexp) for ICU regular expressions.
+*/
 public struct Regex {
     
     private var regexPattern: NSRegularExpression
@@ -20,10 +23,17 @@ public struct Regex {
         }
     }
     
+    /// Regex pattern used to match.
+    /// - Returns: `String` format of pattern
     public func toString() -> String {
         return regexPattern.pattern
     }
     
+    /**
+     Finds regex matches in a string.
+     - Parameter str: the `String` to find matches in
+     - Returns: A `Match` based on the matching result
+     */
     internal func matchWithPattern(str: String) -> Match {
         let results = regexPattern.matchesInString(str, options: .ReportCompletion, range: NSRange(location: 0, length: str.startIndex.distanceTo(str.endIndex)))
         var components = [(String, Range<String.Index>)]()
@@ -34,28 +44,43 @@ public struct Regex {
         return Match(components: components)
     }
     
+    /**
+     Replaces regex matches in string.
+     - Parameter str: the `String` to find matches in
+     - Parameter replacement: `String` to replace in the matches
+     - Returns: A new `String` with the matches replaced
+     */
     internal func replaceMatchesInString(str: String, replacement: String) -> String {
         let replaceString = NSMutableString(string: str)
         regexPattern.replaceMatchesInString(replaceString, options: .ReportCompletion, range: NSRange(location: 0, length: str.startIndex.distanceTo(str.endIndex)), withTemplate: replacement)
         return replaceString as String
     }
     
+    /**
+     Searches the string to find any match.
+     - Parameter str: the `String` to find a match in
+     - Returns: An optional `Int` of the location of the match
+     */
     internal func search(str: String) -> Int? {
         if let result = regexPattern.firstMatchInString(str, options: .ReportCompletion, range: NSRange(location: 0, length: str.startIndex.distanceTo(str.endIndex))) {
-            result.range.location
+            return result.range.location
         }
         return nil
     }
     
+    /// The `Match` struct holds the values of a regex match from pattern matching with a string. Uses its methods to access different values of the matching result.
     public struct Match {
+        /// A collection of a tuple object with a value of the substring and the range of that substring from the string it was matched in/
         public let components: [(String, Range<String.Index>)]
         
+        ///  A collection of just the substrings that were found in the match.
         public func subStrings() -> [String] {
             return components.map {
                 $0.0
             }
         }
         
+        ///  A collection of just the substring ranges that were found in the match.
         public func ranges() -> [Range<String.Index>] {
             return components.map {
                 $0.1
@@ -70,33 +95,44 @@ public prefix func <> (pattern: String) -> Regex? {
     return Regex(pattern: pattern)
 }
 
-infix operator ==~ {}
+infix operator =~ {}
 
-public func ==~ (input: String, regex: Regex) -> Bool {
-    if let match = regex.search(input) where match > 0 {
-        return true
-    }
-    return false
+public func =~ (input: String, regex: Regex) -> Bool {
+    return regex.search(input) != nil
 }
 
-public func ==~ (input: String, regexPatternStr: String) -> Bool {
+public func =~ (input: String, regexPatternStr: String) -> Bool {
     if let regex = Regex(pattern: regexPatternStr) {
-        if let match = regex.search(input) where match > 0 {
-            return true
-        }
+        return regex.search(input) != nil
     }
     return false
 }
 
 extension String {
+    /**
+     Finds regex matches in the string. This method will find multiple matches in the string.
+     - Parameter regex: `Regex` object that holds the pattern to find matches with
+     - Returns: A `Match` based on the matching result
+     */
     public func match(regex: Regex) -> Regex.Match {
         return regex.matchWithPattern(self)
     }
     
+    /**
+     Finds regex matches in the string and replaces those matches with the replacement string.
+     - Parameter regex: `Regex` object that holds the pattern to find matches with
+     - Parameter withString: The replacement `String` to insert in place of the match
+     - Returns: A new `String` with the replacements applied
+     */
     public func replace(regex: Regex, withString: String) -> String {
         return regex.replaceMatchesInString(self, replacement: withString)
     }
     
+    /**
+     Searches the string to find any match.
+     - Parameter regex: `Regex` object that holds the pattern to find matches with
+     - Returns: An optional `Int` of the location of the match
+     */
     public func search(regex: Regex) -> Int? {
         return regex.search(self)
     }
